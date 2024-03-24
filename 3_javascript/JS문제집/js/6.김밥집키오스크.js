2ea3090e760f10ce6d3bfeda5214df9d467dff3d
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-03-24 21:53:45 +0900

    키오스크

--- 3_javascript/JS문제집/js/6.김밥집키오스크.js
+++ 3_javascript/JS문제집/js/6.김밥집키오스크.js
@@ -1,7 +1,7 @@
 const empty = document.getElementById("empty-cart");
 const cart = document.getElementById("cart");
 const total = document.getElementById("total");
-
+let totalPrice = 0;
 // 버튼 눌렀을 때 매개변수 2개 이용하여 값 가져오기
 function addToCart(menu, price) {
     // cart 태그 안에 div 만들어서 안에 내용 넣기
@@ -11,7 +11,15 @@ function addToCart(menu, price) {
     cartItem.innerHTML = menu;
     cart.append(cartItem);
 
-    all += price;
-    total.innerHTML = all;
-    // total.innerHtml += price;
+    // all += price;
+    // total.innerHTML = all;
+    
+    totalPrice = totalPrice + price;
+    
+    total.innerText = totalPrice;
+
+    
+    console.log(price);
+    console.log(totalPrice);
+
 }
